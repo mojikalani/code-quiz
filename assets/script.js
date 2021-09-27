b581269c634eddf14005c17e33bacba157d462e7
kalanimojica <mojica.kalani@gmail.com>
2021-09-26 19:32:50 -0700

    JS template starter

--- assets/script.js
+++ assets/script.js
@@ -12,30 +12,74 @@ var gameEndEl = document.querySelector("#gameEnd");
 var scoreEl = document.querySelector("#score"); 
 var initialsEl = document.querySelector("#initials");
 var submitButtonEl = document.querySelector("#submitButton"); 
+var instructionsEl= document.querySelector(".instructions");
+
 
 // Javascript Elements
-var secondsLeft; 
+var secondsLeft; //Will change based on answers later
 var score= 0; 
 var leaderBoard = [];
 var timer; 
 
+//Array for holding all questions
+
+var questionsPool = [
+    {
+        question: "Question 1", 
+        multipleChoice: [], 
+        answer: ""
+    },
+    {
+        question: "Question 2", 
+        multipleChoice: [], 
+        answer: ""
+    },
+    {
+        question: "Question 3", 
+        multipleChoice: [], 
+        answer: ""
+    },
+    {
+        question: "Question 4", 
+        multipleChoice: [], 
+        answer: ""
+    },
+    {
+        question: "Question 5", 
+        multipleChoice: [], 
+        answer: ""
+    }
+];
+
 // Starting the quiz
 function startQuiz() { 
     
-    secondsLeft = 10;
+    secondsLeft = 10; //will change later
     startTimer(); 
+    //instructionsEl.setAttribute("class", "hide"); //reference to hiding instructions 
 }
 startButtonEl.addEventListener("click", startQuiz);
+
 // ---- Timer Function ----
 
 function startTimer(){ 
     timer = setInterval(function() { 
        if (secondsLeft <=0 ){ 
-           clearInterval();
+           clearInterval(timer);
        }else { 
            timerEl.textContent = secondsLeft; 
            secondsLeft--;
        }
    }, 1000)
    
+}
+
+// ---- Getting a question from the pool ----
+function quesPool () { 
+
+}
+
+// ---- Ending the game ----
+function endGame() { 
+    
 }
